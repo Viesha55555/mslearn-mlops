86a400718789b977934638df27c6ee77d1e1f756
Viesturs Celms <vcelms@gmail.com>
2023-07-24 13:37:07 +0300

    commiting

--- src/model/train.py
+++ src/model/train.py
@@ -13,7 +13,7 @@ from sklearn.linear_model import LogisticRegression
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
+    # TO DO: enable autologging y
     mlflow.autolog()
 
     # read data
