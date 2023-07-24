0b30d90fbbbe621033455f7cb7bcddc17e627477
Viesturs Celms <vcelms@gmail.com>
2023-07-24 12:49:47 +0300

    adding comment

--- src/model/train.py
+++ src/model/train.py
@@ -50,7 +50,7 @@ def split_data(df):
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+#comment here
 
 def parse_args():
     # setup arg parser
